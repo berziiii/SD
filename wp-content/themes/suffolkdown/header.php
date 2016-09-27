6fa24939de08a6b53f91c274819b144f66017956
berziiii <brian@offpistedesigns.com>
2016-09-27 15:22:30 -0400

    Fluid nav bar success
    -Change css to allow for fluid nav bar.

--- wp-content/themes/suffolkdown/header.php
+++ wp-content/themes/suffolkdown/header.php
@@ -21,17 +21,21 @@
 <body <?php body_class(); ?>>
 	<div id="page" class="hfeed site">
 		<header id="masthead" class="site-header" role="banner">
-			<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-				<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
-				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-			</a>
+			<div class="site-image-container">
+				<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+					<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
+					<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+				</a>
+			</div>
 			<div id="navbar" class="navbar">
-				<nav id="site-navigation" class="navigation main-navigation" role="navigation">
-					<button class="menu-toggle"><?php _e( 'Menu', 'suffolkdowns' ); ?></button>
-					<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'suffolkdowns' ); ?>"><?php _e( 'Skip to content', 'suffolkdowns' ); ?></a>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu', 'menu_id' => 'primary-menu' ) ); ?>
-					<!-- <?php get_search_form(); ?> -->
-				</nav>
+				<div class="site-nav-container">
+					<nav id="site-navigation" class="navigation main-navigation" role="navigation">
+						<button class="menu-toggle"><?php _e( 'Menu', 'suffolkdowns' ); ?></button>
+						<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'suffolkdowns' ); ?>"><?php _e( 'Skip to content', 'suffolkdowns' ); ?></a>
+						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu', 'menu_id' => 'primary-menu' ) ); ?>
+						<!-- <?php get_search_form(); ?> -->
+					</nav>
+				</div>
 			</div>
 		</header>
-		<div id="main" class="site-main">
+		<div id="main" class="site-main clear-float">
