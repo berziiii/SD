2e34575544d026ffee2039711a4a2c4861aaa933
berziiii <brian@offpistedesigns.com>
2016-10-03 09:28:40 -0400

    Clean up css
    -Clean up css file to remove all unused or commented out code
    -Move media query lines to separate file.

--- wp-content/themes/suffolkdown/header.php
+++ wp-content/themes/suffolkdown/header.php
@@ -17,6 +17,7 @@
 	<meta name="viewport" content="width=device-width">
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri() . "/assets/styles/suffolkdown.css"; ?>">
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri() . "/assets/bootstrap-3.3.5-dist/css/bootstrap.min.css"; ?>">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() . "/assets/styles/media.css"; ?>">
 	<script type="text/javascript" src="<?php echo get_template_directory_uri() . "/assets/scripts/jquery-3.1.1.min.js"; ?>"></script>
 	<script type="text/javascript" src="<?php echo get_template_directory_uri() . "/assets/scripts/mkoryak-floatThead/dist/jquery.floatThead.min.js"; ?>"></script>
 	<script type="text/javascript" src="<?php echo get_template_directory_uri() . "/assets/bootstrap-3.3.5-dist/js/bootstrap.min.js"; ?>"></script>
